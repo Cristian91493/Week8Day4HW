c135c4b9c4dfe23954c1aa763d3b3d94edec062e
Cristian <cristian91493@gmail.com>
2024-01-02 14:44:58 -0500

    second commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+
+
+function Homepage(props) {
+    return (
+        <div>
+            <h1>Welcome to the home page</h1>
+            <h1>Click on the Stock List for a list of companies</h1>
+            <h1>Then click on a company for up to date information on their stock</h1>
+        </div>
+    );
+}
+
+export default Homepage;
---
+++ src/pages/StockPage.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import Data from "../data"
+
+import { useParams } from "react-router-dom";
+
+function StockPage(props) {
+
+    let [stock, setStock] = useState({});
+    let { symbol } = useParams();
+    console.log(symbol);
+
+
+    async function getStock() {
+        let response = await fetch(`https://financialmodelingprep.com/api/v3/profile/${symbol}?apikey=S7hvBLsZn8FWd6leuhE28EV24zXB5sgu`)
+        let data = await response.json()
+        console.log(data);
+        setStock(data[0]);
+    }
+
+    useEffect(() => { getStock() }, [])
+
+    return (
+        <div className="stock">
+            <h1>{stock.companyName}</h1>
+            <section className="stock-info">
+                <h2>CEO: {stock.ceo}</h2>
+                <h2>Changes: {stock.changes}</h2>
+                <h2>Price: ${stock.price}</h2>
+                <h2>Symbol: {stock.symbol}</h2>
+            </section>
+        </div>
+    );
+}
+
+export default StockPage;
